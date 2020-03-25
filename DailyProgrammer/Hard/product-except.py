2a3208c682613815fea6df82e8270436dbb31bb9
Bleck <OverlordBleck@gmail.com>
2020-03-25 08:35:01 -0500

    tidy up product-except

--- DailyProgrammer/Hard/product-except.py
+++ DailyProgrammer/Hard/product-except.py
@@ -1,20 +1,20 @@
 array = [1,2,3,4,5]
 
-def productExcept(array):
+def productSumExcept(array):
     output = []
 
     for i in range(len(array)):
         product_sum = 1
 
         for j in range(len(array)):
-            num = array[j]
             if i == j:
                 continue
 
+            num = array[j]
             product_sum *= num
 
         output.append(product_sum)
 
     return output
 
-print(productExcept(array))
+print(productSumExcept(array))
